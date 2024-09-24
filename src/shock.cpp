9e3caa2ce2aee85f997b32b30c8fd5ade71cb90a
Benjamin <bbabik@pm.me>
2024-09-24 21:26:57 +0200

    Added gitignore and shock.cpp

---
+++ src/shock.cpp
@@ -0,0 +1,12 @@
+#include "Kadet.hpp"
+
+namespace KaDet
+{
+    namespace shock
+    {
+        int eq_calc(std::shared_ptr<Ct::Solution> gas1, std::shared_ptr<Cantera::Solution> gas2, double errT, double errV)
+        {
+            return 0;
+        }
+    }
+}
