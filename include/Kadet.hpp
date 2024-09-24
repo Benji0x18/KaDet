6a5a98302069f0af325a8691e3d542540328893b
Benjamin <bbabik@pm.me>
2024-09-24 12:48:16 +0200

    Added codebase modified readme and finished CMakeLists

---
+++ include/Kadet.hpp
@@ -0,0 +1,16 @@
+/*
+    This is a header of the Shock and Detonation Library KaDet. Whenever using KaDet make sure to include this header in the file.
+    
+*/
+#include <iostream>
+#include <cmath>
+#include "cantera/core.h"
+
+namespace Ct = Cantera;
+
+namespace KaDet
+{
+    
+
+
+}
